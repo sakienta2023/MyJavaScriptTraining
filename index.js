6aa70f784dac6f21b78f57d35b728e14247a9338
sakienta2023 <127915543+sakienta2023@users.noreply.github.com>
2024-02-12 17:06:35 +0300

    updated new files

---
+++ index.js
@@ -0,0 +1,14 @@
+// console.log('Hello');
+// console.log('I like pizza');
+// window.alert('This is an alert');
+// window.alert('I like Pizza');
+
+document.getElementById("myH1").textContent = 'Hello';
+document.getElementById("myP").textContent = 'Hello';
+
+// This is a comment
+
+/* This
+is 
+a 
+comment */
